1332faedfebc1dc647000f865cabc3c323290aae
Alessandro Crescenzi <centralcano>
2025-09-23 14:01:29 +0200

    test di git

---
+++ script.js
@@ -0,0 +1,49 @@
+const container = document.querySelector('.container');
+
+const apps = [
+
+      banchi = {
+            display: "Banchi",
+            id: "banchi",
+      },
+      crazy_gamble = {
+            display: "Crazy Gamble",
+            id: "crazy-gamble",
+      },
+      tris = {
+            display: "Tris",
+            id: "tris",
+      },
+      indovina_il_numero = {
+            display: "Indovina il Numero",
+            id: "indovina-il-numero",
+      },
+      talpe = {
+            display: "Talpe",
+            id: "talpe",
+      },
+      cursori = {
+            display: "Cursori",
+            id: "cursori",
+      },
+      calcolatore_nether = {
+            display: "Calcolatore Nether",
+            id: "calcolatore-nether",
+      },
+]
+
+apps.forEach(app => {
+
+      const appButton = document.createElement('a');
+      appButton.classList.add('app');
+
+      const appButtonSpan = document.createElement('span');
+      appButton.appendChild(appButtonSpan);
+      appButtonSpan.textContent = app.display;
+
+      appButton.style.backgroundImage = "url(apps/" + app.id + "/cover.png)";
+      
+      appButton.href = "apps/" + app.id + "/index.html";
+      
+      container.appendChild(appButton);
+})
